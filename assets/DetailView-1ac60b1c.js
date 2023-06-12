9b2973f6ec87ae445a2f844d954cd49a85bf8a7b
zechong.zou <zechong.zou@brilliant-solution.com>
2023-06-12 23:47:09 +0800

    dist

---
+++ assets/DetailView-1ac60b1c.js
@@ -0,0 +1,1 @@
+import{_ as e,o as c,c as t}from"./index-4e88a245.js";const o={},s={class:"detail-view"};function a(n,r){return c(),t("div",s," 详情 ")}const i=e(o,[["render",a]]);export{i as default};
