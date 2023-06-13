68cc151b7dc339279be79d0b72aa2337ee0c70b5
zechong.zou <zechong.zou@brilliant-solution.com>
2023-06-13 23:47:19 +0800

    update

--- assets/OrdersView-971b3811.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o as c,c as r}from"./index-4e88a245.js";const s={},o={class:"orders-view"};function t(n,_){return c(),r("div",o," 清单 ")}const d=e(s,[["render",t]]);export{d as default};
